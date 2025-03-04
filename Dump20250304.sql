b2361f86fe97c98e416ecdb3c21dbac5753c1daa
lina <160572925+d0rlina@users.noreply.github.com>
2025-03-04 17:05:38 +0700

    Update Dump20250304.sql

--- Dump20250304.sql
+++ Dump20250304.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `clients`
 --
 
-DROP TABLE IF EXISTS `clients`;
+DROP TABLE IF EXISTS `NECLIENT`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clients` (
